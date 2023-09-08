24560b7dceed1dee1dfd8ba950cc537147909dfe
parthipanc143 <144426707+parthipanc143@users.noreply.github.com>
2023-09-08 13:07:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-#Online Python compiler (interpreter) to run Python online. 
- 
-def fact_rec(n):
-     if n==0 or n==1:
-          return 1
-     else:
-         return n*fact_rec(n-1)
+# Leap year
 
-number = 2
-res = fact_rec(number)
+"""
+year % 4 == 0 &
+year % 100 != 0 /
+year % 400 == 0
 
-print("The factorial of {} is {}.".format(number,res))
+"""
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True 
+  else:
+    return False
+
+year = 2012
+
+if isLeapYear(year):
+ print('{} is a leap year.'.format(year))
+else:
+ print('{} is not a leap year.'.format(year))
